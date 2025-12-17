ee7651a84c80f92802d8b4eccfd9482480ec7378
Hiunehihi <nguyenhieuhocgioi@gmail.com>
2025-12-17 19:07:53 +0700

    style: Sửa lại giao diện nền sáng

--- app/src/main/java/com/example/cafemanagementservices/customer/CustomerHomeActivity.java
+++ app/src/main/java/com/example/cafemanagementservices/customer/CustomerHomeActivity.java
@@ -1,13 +1,10 @@
 package com.example.cafemanagementservices.customer;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
